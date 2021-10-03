b8a7e100ee9226af19c3dcf09610dcb45afa25c1
Mech <mechsix@users.noreply.github.com>
2021-10-03 20:29:42 +0800

    feat: Make main.js get DB connection info from environment variables

--- main.js
+++ main.js
@@ -1,33 +1,27 @@
-var MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient
 
-//Create a MongoDB client, open a connection to DocDB; as a replica set,
-//  and specify the read preference as secondary preferred
+const DB_USER = process.env.DB_USER
+const DB_PASS = process.env.DB_PASS
+const DB_HOST = process.env.DB_HOST
+const DB_NAME = process.env.DB_NAME
 
-var client = MongoClient.connect(
-'mongodb://<sample-user>:<password>@sample-cluster.node.us-east-1.docdb.amazonaws.com:27017/sample-database?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false',
-{
-  tlsCAFile: `rds-combined-ca-bundle.pem` //Specify the DocDB; cert
-},
-function(err, client) {
-    if(err)
-        throw err;
+const client = MongoClient.connect(
+    `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}`,
+    // {
+    //   tlsCAFile: `rds-combined-ca-bundle.pem` //Specify the DocDB; cert
+    // },
+    (err, client) => {
+        if(err) {
+            throw err;
+        }
 
-    //Specify the database to be used
-    db = client.db('sample-database');
+        db = client.db(DB_NAME);
 
-    //Specify the collection to be used
-    col = db.collection('sample-collection');
+        db.listCollections().toArray((getErr, coll) => {
+            console.log(">>>", coll)
+        })
 
-    //Insert a single document
-    col.insertOne({'hello':'Amazon DocumentDB'}, function(err, result){
-      //Find the document that was previously written
-      col.findOne({'hello':'DocDB;'}, function(err, result){
-        //Print the result to the screen
-        console.log(result);
-
-        //Close the connection
         client.close()
-      });
-   });
-});
+    }
+);
 
